91a743420c8cab98fa1a229717e4645d4511d7c4
Waffle <jaxonjjohnson@gmail.com>
2022-06-06 17:42:33 -0400

    fixed delete and added put stub

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -14,6 +14,15 @@ breads.get('/', (req, res) => {
 
 
 module.exports = breads
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+  
+})
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
@@ -41,4 +50,17 @@ breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+
+
 
