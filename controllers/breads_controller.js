5dfdc24554c20dd12c67638ae3a4273199c0093a
Waffle <jaxonjjohnson@gmail.com>
2022-06-11 10:18:46 -0400

    changed edit and show routes to match MongoDB

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -18,13 +18,16 @@ breads.get('/', (req, res) => {
 module.exports = breads
 
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('edit', {
+        bread: foundBread
+      })
+    })
   })
   
-})
+
 // NEW
 breads.get('/new', (req, res) => {
   res.render('new')
@@ -59,20 +62,26 @@ breads.post('/', (req, res) => {
 
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id)
+    .then(deletedBread => {
+      res.status(303).redirect('/breads')
+    })
 })
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+breads.put('/:id', (req, res) => {
   if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
+    .then(updatedBread => {
+      console.log(updatedBread) 
+      res.status(204).redirect(`/breads/${req.params.id}`) 
+    })
 })
+
 
 
 
